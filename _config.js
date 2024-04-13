ad97d0a54c9a75a8e24549540642b28f181dbb25
MoSam007 <samato.moma@gmail.com>
2024-04-13 16:04:41 +0300

    Update config file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://samato:n70Lo8MZgJbCmnTu@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://samato:n70Lo8MZgJbCmnTu@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://samato:n70Lo8MZgJbCmnTu@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://samato:n70Lo8MZgJbCmnTu@cluster1.qjvlrbu.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://samato:n70Lo8MZgJbCmnTu@cluster1.qjvlrbu.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://samato:n70Lo8MZgJbCmnTu@cluster1.qjvlrbu.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
